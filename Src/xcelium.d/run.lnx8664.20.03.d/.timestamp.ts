ad9eaa79b47d6ea30910c3aeba105b7cc827c97c
olkmphy <nam.nguyenolkmphy@hcmut.edu.vn>
2024-02-24 16:59:54 +0700

    Adding files

--- Src/xcelium.d/run.lnx8664.20.03.d/.timestamp.ts
+++ Src/xcelium.d/run.lnx8664.20.03.d/.timestamp.ts
@@ -1,2 +1,4 @@
-1708675292 /data/vlsi2022/cc02group5/2153599/work/simulation_env/bound_flasher.v
-1708676650 /data/vlsi2022/cc02group5/2153599/work/simulation_env/test_bench.v
+1708766529 /data/vlsi2022/cc02group5/2153599/work/simulation_env/bound_flasher.v
+1708768711 /data/vlsi2022/cc02group5/2153599/work/simulation_env/test_bench.v
+1708689016 /data/vlsi2022/cc02group5/2152994/work/simulation_env/test_bench.v
+1708680614 /data/vlsi2022/cc02group5/2152994/work/simulation_env/bound_flasher.v
